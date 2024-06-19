682def4c1785e215cca0015a7717aea9a017fd8a
CLR1997 <colinflinke@gmail.com>
2024-06-19 22:44:16 +0200

    Final

--- STARGAZER.R
+++ STARGAZER.R
@@ -1,8 +1,6 @@
 #### stargazer!
 
 
-library(stargazer)
-library(knitr)
 Sem1_to <- c("Sem 2", "Sem 3", "Sem 4")
 kappa_value <- c(0.64, 0.70, 0.67)
 #t(data.frame(Sem1_to, kappa_value))
@@ -28,12 +26,65 @@ cowplot::plot_grid(residuals.AR1, residuals.Toelpitz, residuals.ARMA11, residual
 cowplot::plot_grid(residuals.Unstructured, residuals.CompSymm, residuals.Exponential, residuals.Gaussian)
 
 stargazer(Anova(pseudo.trajectory.ARMA11), summary = FALSE, flip = F, title = "Significance Pseudo Trajectory")
-stargazer(full.model.ARMA11, title = "Summary Full Model", type = "text", font.size = "small")
+stargazer(full.model.ARMA11, small.model.ARMA11, title = "Summary 'main' Model effects", type = "latex", font.size = "small")
 
 plot(effect("Semester*Cluster_current", full.model.ARMA11, robust= "hc3", correction = "Sidak"), main = "Full Model: Cluster & Semester")
 plot(effect("Cluster_current*Semester", small.model.ARMA11, robust= "hc3", correction = "Sidak"), main = "Small Model: Cluster & Semester")
 plot(effect("Cluster_current*Time", time.slope.ARMA11, robust= "hc3", correction = "Sidak"), main = "Time Model: Cluster & Semester")
 
+plot(effect("Sex", full.model.ARMA11, robust= "hc3", correction = "Sidak"), main = "Effect of Gender")
+plot(effect("Fager4_binary", full.model.ARMA11, robust= "hc3", correction = "Sidak"), main = "Effect of Smoking")
+
+plot(effect("Group_transition1", pseudo.trajectory.ARMA11, robust= "hc3", correction = "Sidak"), main = "Effect Cluster Transition")
+
+ggplot.fm
+ggplot.sm
+ggplot.pt
+
+range.ri <- data.frame(full.model = as.vector(range(ranef(full.model.ARMA11))),
+                       small.model = as.vector(range(ranef(small.model.ARMA11))),
+                       pseudo.trajectory = as.vector(range(ranef(pseudo.trajectory.ARMA11))))
+
+rownames(range.ri) <- c("min", "max")
+range.ri
+
+stargazer(range.ri) ## I've plugged in the values in the table, because they were to small, the function rounded to zero
+# max(as.vector(range(ranef(full.model.ARMA11)))) > 0.000001
+# [1] TRUE
+# max(as.vector(range(ranef(full.model.ARMA11)))) > 0.000001
+# [1] TRUE
+# 
+
+stargazer(vif(full.model.gls))
+stargazer(vif(pseudo.trajectory.gls))
 
 
-library(broom)
+
+stargazer(df_scores_sm.r2[,-3], summary = FALSE)
+stargazer(df_scores_fmri.r2[,-3], summary = FALSE)
+
+
+stargazer(df_interaction[,-3], summary = FALSE)
+
+stargazer(sm.outlier.barebones, fm.outlier.barebones, fm.outlier.int)
+
+range.numeric <- data.frame(full.model = as.vector(range(ranef(fm.outlier.barebones))),
+                       small.model = as.vector(range(ranef(sm.outlier.barebones))))
+rownames(range.numeric) <- c("min", "max")
+range.numeric
+stargazer(range.numeric) ## I had to adjust the table a lot manually. 
+
+ggplot.fmo + ggtitle("Numerical Full Model residuals vs fitted")
+ggplot.smo + ggtitle("Numerical Small Model residuals vs fitted")
+
+r.squaredGLMM(fm.outlier.barebones)
+
+
+stargazer(df.sm.spline[,-3], summary = FALSE)
+
+stargazer(sm.splines.mix)
+summary(sm.splines.mix$lme)
+plot.gam(sm.splines.mix$gam, all.terms = TRUE, main = "Marijuana consumption spline function")
+ggplot.mix + ggtitle("GAMM residuals vs fitted")
+
+range(random.effects(sm.splines.mix$lme))
---
+++ legacy code/script_for_NAdata_comp.R
@@ -0,0 +1,162 @@
+### doing the same wihtout correcting GPA
+
+rm(list=ls())
+library(tidyverse) 
+library(readspss) 
+library(tableone)
+library(ggiraph)
+library(cowplot)
+library(patchwork)
+library(nlme)
+library(lme4)
+library(car)
+library(lattice)
+library(effects)
+library(sjPlot)
+library(lmerTest)
+library(sandwich)
+library(pbkrtest)
+data.file.100000 <- read.sav("BARCS_Data_used_for_paper.sav")
+data.sem1 <- data.file.100000 %>% filter(Semester == 1)
+data.sem2 <- data.file.100000 %>% filter(Semester == 2)
+data.sem3 <- data.file.100000 %>% filter(Semester == 3)
+data.sem4 <- data.file.100000 %>% filter(Semester == 4)
+
+data.SEM1 <- data.sem1 %>% mutate(LOG_Avg_Drinks_current =  LOG_Avg_Drinks_SEM1, 
+                                  LOG_Avg_MJ_current = LOG_Avg_MJ_SEM1,
+                                  Cluster_current = Cluster_SEM1,
+                                  Log_B30_current = Log_B30_Sem1)  
+data.SEM2 <- data.sem2 %>% mutate(LOG_Avg_Drinks_current =  LOG_Avg_Drinks_SEM2, 
+                                  LOG_Avg_MJ_current = LOG_Avg_MJ_SEM2,
+                                  Cluster_current = Cluster_SEM2,
+                                  Log_B30_current = Log_B30_Sem2)  
+data.SEM3 <- data.sem3 %>% mutate(LOG_Avg_Drinks_current =  LOG_Avg_Drinks_SEM3, 
+                                  LOG_Avg_MJ_current = LOG_Avg_MJ_SEM3,
+                                  Cluster_current = Cluster_SEM3,
+                                  Log_B30_current = Log_B30_Sem3) 
+data.SEM4 <- data.sem4 %>% mutate(LOG_Avg_Drinks_current =  LOG_Avg_Drinks_SEM4, 
+                                  LOG_Avg_MJ_current = LOG_Avg_MJ_SEM4,
+                                  Cluster_current = Cluster_SEM4,
+                                  Log_B30_current = Log_B30_Sem4) 
+
+data.file.100000.long <- rbind(data.SEM1, data.SEM2, data.SEM3, data.SEM4)
+
+data.file.100000.long <- data.file.100000.long %>%
+  mutate(Avg_Drinks_SEM1 = 10^(LOG_Avg_Drinks_SEM1) -1,
+         Avg_Drinks_SEM2 = 10^(LOG_Avg_Drinks_SEM2) -1,
+         Avg_Drinks_SEM3 = 10^(LOG_Avg_Drinks_SEM3) -1,
+         Avg_Drinks_SEM4 = 10^(LOG_Avg_Drinks_SEM4) -1,
+         Avg_Drinks_current = 10^(LOG_Avg_Drinks_current) -1,
+         Avg_MJ_SEM1 = 10^(LOG_Avg_MJ_SEM1) -1,
+         Avg_MJ_SEM2 = 10^(LOG_Avg_MJ_SEM2) -1,
+         Avg_MJ_SEM3 = 10^(LOG_Avg_MJ_SEM3) -1,
+         Avg_MJ_SEM4 = 10^(LOG_Avg_MJ_SEM4) -1,
+         Avg_MJ_current = 10^(LOG_Avg_MJ_current) -1,
+         B30_SEM1 = 10^(Log_B30_Sem1) -1,
+         B30_SEM2 = 10^(Log_B30_Sem2) -1,
+         B30_SEM3 = 10^(Log_B30_Sem3) -1,
+         B30_SEM4 = 10^(Log_B30_Sem4) -1,
+         B30_current = 10^(Log_B30_current) -1
+  )
+
+### needs some work
+data.file.100000.long <- data.file.100000.long %>%
+  group_by(BARCS_ID) %>%
+  mutate(diff_Avg_Drinks_current = Avg_Drinks_current - lag(Avg_Drinks_current, 1),
+         diff_Avg_MJ_current = Avg_MJ_current - lag(Avg_MJ_current, 1),
+         diff_B30_current = B30_current - lag(B30_current, 1),
+         diff_LOG_Avg_Drinks_current = LOG_Avg_Drinks_current - lag(LOG_Avg_Drinks_current, 1),
+         diff_LOG_Avg_MJ_current = LOG_Avg_MJ_current - lag(LOG_Avg_MJ_current, 1),
+         diff_GPA = GPA - lag(GPA, 1),
+         diff_LOG_B30_current = Log_B30_current - lag(Log_B30_current, 1),
+         transition_current = as.numeric(Cluster_current) - lag(as.numeric(Cluster_current), 1)
+  )
+
+data.file.100000.long <- data.file.100000.long %>%
+  ungroup() %>%
+  group_by(Semester) %>%
+  mutate(mean_Avg_Drinks = Avg_Drinks_current - mean(Avg_Drinks_current),
+         std_Avg_Drinks = mean_Avg_Drinks / sd(Avg_Drinks_current),
+         mean_LOG_Avg_Drinks = LOG_Avg_Drinks_current - mean(LOG_Avg_Drinks_current),
+         std_LOG_Avg_Drinks = mean_LOG_Avg_Drinks / sd(LOG_Avg_Drinks_current),
+         mean_Avg_MJ = Avg_MJ_current - mean(Avg_MJ_current, na.rm = TRUE),
+         std_Avg_MJ = mean_Avg_MJ / sd(Avg_MJ_current, na.rm = TRUE),
+         mean_LOG_Avg_MJ = LOG_Avg_MJ_current - mean(LOG_Avg_MJ_current, na.rm = TRUE),
+         std_LOG_Avg_MJ = mean_LOG_Avg_MJ / sd(LOG_Avg_MJ_current, na.rm = TRUE),
+         mean_B30 = B30_current - mean(B30_current, na.rm = TRUE),
+         std_B30 = mean_B30 / sd(B30_current, na.rm = TRUE),
+         mean_LOG_B30 = Log_B30_current - mean(Log_B30_current, na.rm = TRUE),
+         std_LOG_B30 = mean_LOG_B30 / sd(Log_B30_current, na.rm = TRUE)#,
+         # mean_GPA = GPA - mean(GPA),
+         # std_GPA = mean_GPA / sd(GPA) there are NA's in the data that were wrongly assigned the value 0
+  )
+
+data.file.100000.long <- data.file.100000.long %>%
+  relocate(BARCS_ID, Semester, GPA, diff_GPA, Avg_Drinks_current, mean_Avg_Drinks, std_Avg_Drinks, diff_Avg_Drinks_current, 
+           LOG_Avg_Drinks_current, mean_LOG_Avg_Drinks, std_LOG_Avg_Drinks, diff_LOG_Avg_Drinks_current, Avg_MJ_current, 
+           mean_Avg_MJ, std_Avg_MJ, diff_Avg_MJ_current, LOG_Avg_MJ_current, mean_LOG_Avg_MJ, std_LOG_Avg_MJ, 
+           diff_LOG_Avg_MJ_current)
+
+## Cluster and student id need to be factors 
+data.file.100000.long <- data.file.100000.long %>% mutate(Cluster_current = as.factor(Cluster_current),
+                                            BARCS_ID = as.factor(BARCS_ID))
+
+
+## Data selection required to identify if GPA = 0 and SAT = 0 are NAs
+ind.gpa0 <- data.file.100000.long %>% filter(GPA == 0) %>% select(BARCS_ID, Semester, GPA)
+ind.gpa0.vector <- unique(ind.gpa0$BARCS_ID)
+ind.gpa0 <- data.file.100000.long %>% filter(BARCS_ID %in% ind.gpa0.vector) %>% group_by(BARCS_ID, Semester)
+ind.sat0 <- data.file.100000.long %>% ungroup() %>% filter(SATTotal == 0) %>% select(BARCS_ID, Semester, SATTotal, GPA)
+
+
+# ### imputing NA for false classification of SAT / GPA = 0 
+# data.file.100000.long <- data.file.100000.long %>% mutate(GPA = replace(GPA, GPA == 0, NA),
+#                                             SATTotal = replace(SATTotal, SATTotal == 0, NA),
+#                                             SATMath = replace(SATMath, SATMath == 0, NA),
+#                                             SATVerbal = replace(SATVerbal, SATVerbal == 0, NA),
+#                                             SATWriting = replace(SATWriting, SATWriting == 0, NA),
+#                                             mean_GPA = GPA - mean(GPA, na.rm = TRUE),
+#                                             std_GPA = mean_GPA / sd(GPA, na.rm = TRUE))
+
+
+### create variable that counts the amount of NAs for each individual student 
+
+data.file.100000.long <- data.file.100000.long %>% group_by(BARCS_ID) %>% 
+  mutate(sum.GPAna = sum(is.na(GPA)))
+
+### include average GPA for each student 
+data.file.100000.long <- data.file.100000.long %>% group_by(BARCS_ID) %>% mutate(average_GPA = mean(GPA))
+#data.file.100000.long <- data.file.100000.long %>% arrange(sum.GPAna, average_GPA)
+
+data.file.100000.long <- data.file.100000.long %>% 
+  mutate(Sex = case_match(Sex, 1 ~ "male", 2 ~ "female", .default = NA),
+         Cluster_current = case_match(Cluster_current, '1' ~ "1st.cluster", '2' ~ "2nd.cluster",
+                                      '3' ~ "3rd.cluster", .default = NA),
+         Cluster_SEM1 = case_match(Cluster_SEM1, '1' ~ "1st.cluster", '2' ~ "2nd.cluster",
+                                   '3' ~ "3rd.cluster", .default = NA),
+         Fager4_binary = case_match(Fager4_binary, 1 ~ "smoker", 0 ~ "non smoker", .default = NA),
+         FH_binary = case_match(FH_binary, 0 ~ "negative", 1 ~ "positive", .default = NA))
+
+
+## splitting the data into the different subsections of missing GPA data  (4 NAs means there is no GPA data -> dropped)
+subset.0nagpas <- data.file.100000.long %>% filter(sum.GPAna == 0) %>% arrange(average_GPA)
+subset.1nagpas <- data.file.100000.long %>% filter(sum.GPAna == 1) %>% arrange(average_GPA)
+subset.2nagpas <- data.file.100000.long %>% filter(sum.GPAna == 2) %>% arrange(average_GPA)
+subset.3nagpas <- data.file.100000.long %>% filter(sum.GPAna == 3) %>% arrange(average_GPA)
+
+
+
+
+
+
+### Group transition compares only the first semester to the last semester!!!!!!!!!
+
+data.file.100000.long <- data.file.100000.long %>% mutate( Time = Semester,
+                                             Semester = as.factor(Semester),
+                                             Group_transition = as.factor(Group_transition),
+                                             Group_transition1 = as.factor(Group_transition1),
+                                             transition_current = as.factor(transition_current))
+
+
+
+
